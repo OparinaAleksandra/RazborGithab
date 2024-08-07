ec7c7169a6b3f2171f0e1e3c2341647b1fc6029c
OparinaAleksandra <opaaleksandra@rambler.ru>
2024-08-07 15:53:18 +0400

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,5 @@
+quantity_zadaniy = 12
+quantity_hours = 1.5
+name = 'Python'
+time = (quantity_hours / quantity_hours)
+print(name, ', всего задач' + ':', quantity_zadaniy, ", затрачено часов:", quantity_hours, ", среднее время выполнения", time, 'часа.')
